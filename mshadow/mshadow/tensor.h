79e62722f7d0e19ad1df271d2e57fa8c867df2e8
CitronMan <liuxg16@mails.tsinghua.edu.cn>
2016-11-11 17:15:29 +0800

    updateforgpu

--- mshadow/mshadow/tensor.h
+++ mshadow/mshadow/tensor.h
@@ -402,10 +402,16 @@ struct Tensor: public TRValue<Tensor<Device, dimension, DType>,
   MSHADOW_XINLINE void Slice_lxg(Tensor<Device, dimension, DType> dst,index_t idx) const {
       Shape<dimension> s = this->shape_;
       for(index_t x = 0; x < s[1]; ++x) {
-          
-          this[0][idx][x] = dst[0][x];
-//          printf("%lf",dst[0][x]);
+          //this[0][idx][x] = dst[0][x];
+          this->dptr_[x]=dst[0][x];
       }
+       
+      
+      //printf("dptr_[0]%lf",this->dptr_[0]);
+      //printf("%lf",this->dptr_[1]);
+
+
+            
   }
 
   /*!\brief implement the assignment of same type */
